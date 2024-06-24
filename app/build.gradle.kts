6ab942ea60e5bcba323ec97159fef396ef364be5
Yunus Emre Kara <yunuskr40@gmail.com>
2024-06-24 17:03:39 +0200

    worked on implementing google maps

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,6 +39,7 @@ android {
     }
     buildFeatures {
         compose = true
+        dataBinding = true
     }
     composeOptions {
         kotlinCompilerExtensionVersion = "1.5.1"
@@ -52,9 +53,7 @@ android {
 
 
 dependencies {
-    // Grundlegende und bereits vorhandene Abhängigkeiten
     implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
@@ -62,6 +61,7 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.lifecycle.runtime.compose.android)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -70,63 +70,36 @@ dependencies {
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
 
-    // Material Design
     implementation("com.google.android.material:material:1.6.1")
 
-    // Architectural Components (ViewModel und LiveData)
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.5.1")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.5.1")
-
-    // Room Database
     val room_version = "2.4.2"
     implementation("androidx.room:room-runtime:$room_version")
     implementation("androidx.room:room-ktx:$room_version")
     kapt("androidx.room:room-compiler:$room_version")
 
-
-    // Coroutines
-    val coroutines_version = "1.6.4" // Empfohlene stabile Version
+    val coroutines_version = "1.6.4"
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:$coroutines_version")
 
-    // Coroutine Lifecycle Scopes
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.5.1")
-
-    // Navigation Components
     implementation("androidx.navigation:navigation-fragment-ktx:2.5.1")
     implementation("androidx.navigation:navigation-ui-ktx:2.5.1")
 
-    // Glide
     implementation("com.github.bumptech.glide:glide:4.13.2")
-    kapt ("com.github.bumptech.glide:compiler:4.13.2")
+    kapt("com.github.bumptech.glide:compiler:4.13.2")
 
-
-    // Google Maps and Location Services
+    implementation("com.google.maps.android:maps-compose:4.3.3")
+    implementation("com.google.maps.android:maps-ktx:4.3.3")
     implementation("com.google.android.gms:play-services-maps:18.0.2")
     implementation("com.google.android.gms:play-services-location:20.0.0")
 
-    // Dagger-Hilt for Dependency Injection
-    // Dagger Core
-
     implementation("com.google.dagger:dagger:2.44")
     kapt("com.google.dagger:dagger-compiler:2.44")
-
-    // Dagger Android
     api("com.google.dagger:dagger-android:2.44")
     api("com.google.dagger:dagger-android-support:2.44")
     kapt("com.google.dagger:dagger-android-processor:2.44")
 
-
-
-    // Easy Permissions
-    implementation ("pub.devrel:easypermissions:3.0.0")
-
-    // Timber
-    implementation ("com.jakewharton.timber:timber:5.0.1")
-
-    // MPAndroidChart
+    implementation("pub.devrel:easypermissions:3.0.0")
+    implementation("com.jakewharton.timber:timber:5.0.1")
     implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
-
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.5.1")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.5.1")
     implementation("androidx.hilt:hilt-navigation-fragment:1.0.0")
 }
--- app/src/main/java/com/example/runyclub/screens/RunScreen.kt
+++ app/src/main/java/com/example/runyclub/screens/RunScreen.kt
@@ -1,27 +1,57 @@
 package com.example.runyclub.screens
 
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import android.Manifest
+import android.content.pm.PackageManager
+import androidx.activity.result.ActivityResultLauncher
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.navigation.NavController
-import com.example.runyclub.widgets.SimpleBottomAppBar
+import androidx.core.content.ContextCompat
+import androidx.navigation.NavHostController
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.*
+import com.google.android.gms.location.LocationServices
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.viewinterop.AndroidView
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMapOptions
+import com.google.android.gms.maps.MapView
 
 @Composable
-fun RunScreen(
-    navController: NavController,
-) {
-    Scaffold (
-        bottomBar = {
-            SimpleBottomAppBar(
-                navController = navController
-            )
+fun RunScreen(navController: NavHostController, requestPermissionLauncher: ActivityResultLauncher<String>) {
+    val context = LocalContext.current
+    val fusedLocationClient = LocationServices.getFusedLocationProviderClient(context)
+    var currentLocation by remember { mutableStateOf(LatLng(0.0, 0.0)) }
+
+    if (ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION)
+        != PackageManager.PERMISSION_GRANTED) {
+        requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+    } else {
+        fusedLocationClient.lastLocation.addOnSuccessListener { location ->
+            if (location != null) {
+                currentLocation = LatLng(location.latitude, location.longitude)
+            }
         }
-    ){ innerPadding ->
-        Text(
-            text = "Run Screen",
-            modifier = Modifier.padding(innerPadding)
-        )
     }
+
+    val cameraPosition = CameraPosition.fromLatLngZoom(currentLocation, 15f)
+
+    AndroidView({ context ->
+        GoogleMapOptions().apply {
+            mapType(1)
+            zoomControlsEnabled(true)
+            compassEnabled(true)
+        }.let { options ->
+            MapView(context, options).apply {
+                getMapAsync { googleMap ->
+                    googleMap.moveCamera(CameraUpdateFactory.newCameraPosition(cameraPosition))
+                    if (ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION)
+                        == PackageManager.PERMISSION_GRANTED) {
+                        googleMap.isMyLocationEnabled = true
+                    }
+                }
+            }
+        }
+    }, modifier = Modifier.fillMaxSize())
 }
