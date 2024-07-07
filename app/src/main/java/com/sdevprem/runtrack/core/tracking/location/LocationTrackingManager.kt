3fa2c471f6238b6e768338e67c5725e150e5d8b6
Yunus Emre Kara <yunuskr40@gmail.com>
2024-07-07 21:06:41 +0200

    refactoring

--- app/src/main/java/com/sdevprem/runtrack/core/tracking/location/LocationTrackingManager.kt
+++
@@ -1,9 +0,0 @@
-package com.sdevprem.runtrack.core.tracking.location
-
-import com.google.android.gms.location.LocationCallback
-
-interface LocationTrackingManager {
-    fun registerCallback(locationCallback: LocationCallback)
-
-    fun unRegisterCallback(locationCallback: LocationCallback)
-}
